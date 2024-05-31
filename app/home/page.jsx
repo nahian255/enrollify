b94851d9e05d7979be916ea13a46587e0539758b
Mutawchid Nahian <nahimn253@gmail.com>
2024-05-31 14:46:11 +0600

    home page route created

---
+++ app/home/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const homePage = () => {
+    return (
+        <div>
+            <h1 className='text-3xl'> Home page </h1>
+        </div>
+    );
+};
+
+export default homePage;
