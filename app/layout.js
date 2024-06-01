0aa516bd78791681859584509e4973fbd6364cdc
Mutawchid Nahian <nahimn253@gmail.com>
2024-06-02 00:21:13 +0600

    navbar start

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        
+        <nav>
+          <Link className="p-4" href={'/'}>Home</Link>
+          <Link href={'about'}>About</Link>
+        </nav>
+        {children}
+        
+        
+    </body>
     </html>
   );
 }
