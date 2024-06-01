c0867c803bbb48d394d809983a16b48f3c530a17
Mutawchid Nahian <nahimn253@gmail.com>
2024-06-01 22:16:14 +0600

    title change

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -4,6 +4,7 @@ const homePage = () => {
     return (
         <div>
             <h1 className='text-3xl'> Home page </h1>
+            <p> </p>
         </div>
     );
 };
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
-<h1> main page</h1>
+    <Link href={'/home'}>Home page</Link>
 
 
     </main>
