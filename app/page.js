e7acd9ea52064816159a04eb3909804e0507b882
Mutawchid Nahian <nahimn253@gmail.com>
2024-06-04 11:13:10 +0600

    pricing plan start

--- app/page.js
+++ app/page.js
@@ -6,55 +6,63 @@ export default function Home() {
   return (
     <div>
       {/* Header part  */}
-     <header className=" md:flex ">
-       <div className=" md:w-1/2 h-96 bg-red-100 content-center justify-items-center">
+      <header className=" md:flex ">
+        <div className=" md:w-1/2 h-96 bg-red-100 content-center justify-items-center">
           <h1 className="text-3xl">exercise is the key to a healthy lifestyle</h1>
           <p> somethig is writting here. yes kfskj skjfoouj  jdjfs</p>
           <Button></Button>
-       </div>
-       <div className="w-1/2 content-center">
-        img
-       </div>
-     </header>
-
-     {/*  benefits */}
-     <div className="bg-green-200 my-10">
-        <h1 className="text-center text-4xl "> Benefits of exercise</h1>
-        <p className="text-center "> lorem skjdljkkkkks       djkdjksjdskdj kjddj jddddddddddddskjdksjdksj     </p>
-        <div className="flex justify-center py-5">     
-        <div className=" md:flex justify-evenly w-1/2 h-40 bg-purple-400"> 
-          <div className="content-center"> 
-             <h1> img </h1>
-             <div>
-              <h1> Healthy Life </h1>
-             </div>
-          </div>
-          <div  className="content-center"> 
-             <h1> img </h1>
-             <div>
-              <h1> Healthy Life </h1>
-             </div>
-          </div>
-          <div className="content-center"> 
-             <h1> img </h1>
-             <div>
-              <h1> Healthy Life </h1>
-             </div>
-          </div>
         </div>
-        </div>
-     </div>
-       
-       {/* get start part */}
-       <div>
-        <div>
+        <div className="w-1/2 content-center">
           img
         </div>
-        <div>
-          <h1> Ready to make a change </h1>
-          <p> something is writting hererj jfkj fsdio sjf oasjifsa ijf ojfso jsojf soj f s </p>
+      </header>
+
+      {/*  benefits */}
+      <div className="bg-green-200 my-10">
+        <h1 className="text-center text-4xl "> Benefits of exercise</h1>
+        <p className="text-center "> lorem skjdljkkkkks       djkdjksjdskdj kjddj jddddddddddddskjdksjdksj     </p>
+        <div className="flex justify-center py-5">
+          <div className=" md:flex justify-evenly w-1/2 h-40 bg-purple-400">
+            <div className="content-center">
+              <h1> img </h1>
+              <div>
+                <h1> Healthy Life </h1>
+              </div>
+            </div>
+            <div className="content-center">
+              <h1> img </h1>
+              <div>
+                <h1> Healthy Life </h1>
+              </div>
+            </div>
+            <div className="content-center">
+              <h1> img </h1>
+              <div>
+                <h1> Healthy Life </h1>
+              </div>
+            </div>
+          </div>
         </div>
-       </div>
+      </div>
+
+      {/* get start part */}
+      <div className="bg-orange-200 my-10 md:flex h-80">
+        <div className="w-1/2"> 
+          img
+        </div>
+        <div className="bg-gray-200 w-1/2 content-center px-5" >
+          <h1 className="text-3xl pb-5"> Ready to make a change </h1>
+          <p className="pb-6"> something is writting hererj jfkj fsdio something is writting hererj jfkj fsdio sjf oasjifsa ijf ojfso jsojf soj f ssomething is writting hererj jfkj fsdio sjf oasjifsa ijf ojfso jsojf soj f s sjf oasjifsa ijf ojfso jsojf soj f s </p>
+          <Button></Button>
+        </div>
+      </div>
+
+      {/*  pricing plan  */}
+
+      <div>
+      <h1 className="text-center text-4xl "> Gym pricing Plan</h1>
+        <p className="text-center "> lorem skjdljkkkkks       djkdjksjdskdj kjddj jddddddddddddskjdksjdksj     </p>
+      </div>
     </div>
   );
 }
