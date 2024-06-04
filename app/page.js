13ea1329fd75ca981ed842834db3215514dcef66
Mutawchid Nahian <nahimn253@gmail.com>
2024-06-04 10:02:39 +0600

    basic benefits parts done

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import Button from "@/components/home/Button";
+import { Container } from "postcss";
 
 
 export default function Home() {
   return (
     <div>
       {/* Header part  */}
-     <header className="flex ">
-       <div className="w-1/2 h-96 bg-red-100 content-center justify-items-center">
+     <header className=" md:flex ">
+       <div className=" md:w-1/2 h-96 bg-red-100 content-center justify-items-center">
           <h1 className="text-3xl">exercise is the key to a healthy lifestyle</h1>
           <p> somethig is writting here. yes kfskj skjfoouj  jdjfs</p>
           <Button></Button>
@@ -14,13 +15,36 @@ export default function Home() {
        <div className="w-1/2 content-center">
         img
        </div>
-     </header>.
+     </header>
 
-     {/*  benefits */}.
-     <div>
-        <h1> Benefits of exercise</h1>
-        <p> lorem skjdljkkkkks       djkdjksjdskdj kjddj jddddddddddddskjdksjdksj           </p>
+     {/*  benefits */}
+     <div className="bg-green-200 my-10">
+        <h1 className="text-center text-4xl "> Benefits of exercise</h1>
+        <p className="text-center "> lorem skjdljkkkkks       djkdjksjdskdj kjddj jddddddddddddskjdksjdksj     </p>
+        <div className="flex justify-center py-5">     
+        <div className=" md:flex justify-evenly w-1/2 h-40 bg-purple-400"> 
+          <div className="content-center"> 
+             <h1> img </h1>
+             <div>
+              <h1> Healthy Life </h1>
+             </div>
+          </div>
+          <div  className="content-center"> 
+             <h1> img </h1>
+             <div>
+              <h1> Healthy Life </h1>
+             </div>
+          </div>
+          <div className="content-center"> 
+             <h1> img </h1>
+             <div>
+              <h1> Healthy Life </h1>
+             </div>
+          </div>
+        </div>
+        </div>
      </div>
+       
     </div>
   );
 }
