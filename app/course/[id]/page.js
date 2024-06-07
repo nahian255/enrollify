993e64ef688c1c274ba82f049acccac454ac8b6f
Mutawchid Nahian <nahimn253@gmail.com>
2024-06-07 07:10:57 +0600

    try to find specipy data

--- app/course/[id]/page.js
+++ app/course/[id]/page.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const courseDetails = ({params}) => {
+const courseDetails = async ({params}) => {
     const {id}= params
+    console.log(params.id) 
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    // console.log(data?.id) 
+
+
+    // const singleData = data.find( ({ id }) => id === id )
+    const singleData = data.find(user => user.id === params.id);
+    console.log(singleData) 
+
+
     return (
         <div>
             <h1> course Details page </h1>
