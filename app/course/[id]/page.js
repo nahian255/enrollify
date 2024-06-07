84ec34c60be6b96332c52d3251a61adab8f5158f
Mutawchid Nahian <nahimn253@gmail.com>
2024-06-07 09:31:25 +0600

    dynamic data loaded

--- app/course/[id]/page.js
+++ app/course/[id]/page.js
@@ -1,23 +1,40 @@
 import React from 'react';
 
+const courses = [
+   {
+    id : 1 ,
+    name :"lmarai",
+
+   },
+   {
+    id : 2 ,
+    name :"Almarai",
+
+   },
+
+        // Add more courses as needed
+];
+
+
 const courseDetails = async ({params}) => {
     const {id}= params
-    console.log(params.id) 
+    // console.log(courses.id) 
 
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
     const data = await res.json();
-    // console.log(data?.id) 
+    console.log(data) 
 
 
     // const singleData = data.find( ({ id }) => id === id )
-    const singleData = data.find(user => user.id === params.id);
-    console.log(singleData) 
+    // const singleData = data.find(user => user.id === params.id);
+    // console.log(singleData) 
 
 
     return (
         <div>
             <h1> course Details page </h1>
             <p> showing the course id number- {id}</p>
+            <p> name : {data.name}</p>
         </div>
     );
 };
