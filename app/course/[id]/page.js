52e9f1f9dd187c6c775c6c006aa6e34a33718d09
Mutawchid Nahian <nahimn253@gmail.com>
2024-06-08 16:46:56 +0600

    make a api using a dame data

--- app/course/[id]/page.js
+++ app/course/[id]/page.js
@@ -1,20 +1,5 @@
 import React from 'react';
 
-const courses = [
-   {
-    id : 1 ,
-    name :"lmarai",
-
-   },
-   {
-    id : 2 ,
-    name :"Almarai",
-
-   },
-
-        // Add more courses as needed
-];
-
 
 const courseDetails = async ({params}) => {
     const {id}= params
@@ -23,12 +8,6 @@ const courseDetails = async ({params}) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
     const data = await res.json();
     // console.log(data) 
-
-
-    // const singleData = data.find( ({ id }) => id === id )
-    // const singleData = data.find(user => user.id === params.id);
-    // console.log(singleData) 
-
 
     return (
         <div>
