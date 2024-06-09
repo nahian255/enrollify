6b0a221215d50ebc6a085fa9989f141ef17edc19
Mutawchid Nahian <nahimn253@gmail.com>
2024-06-09 20:55:21 +0600

    need to mongo connect

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -5,3 +5,4 @@ import courses from '@/data/course.json'
 export async function GET(request) {
     return new Response(JSON.stringify(courses))
 }
+  
--- components/courses/CourseCard.jsx
+++ components/courses/CourseCard.jsx
@@ -3,12 +3,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import pic from '@/public/fitness.jpg'
-// import yogaImage from '../public/yoga.jpg'; // Replace with actual image path
-// import fatLossImage from '../public/fat-loss.jpg'; // Replace with actual image path
+
 
 const CourseCard = ({ title, description, image , id}) => {
     return (
         <div className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform hover:scale-105 duration-300 ease-in-out">
+            {/* <Image src={`/images/${image}`} alt={title} width={300} height={300} className="w-full h-48 object-cover"/> */}
             <Image src={pic} alt={title} width={300} height={300} className="w-full h-48 object-cover"/>
             <div className="p-5">
                 <h2 className="text-2xl font-bold text-gray-800">{title}</h2>
