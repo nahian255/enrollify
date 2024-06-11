113df9b41817a284de85ac85e02045fbc4450520
Mutawchid Nahian <nahimn253@gmail.com>
2024-06-11 18:33:30 +0600

    make right schema

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,15 +1,20 @@
 
 
 import { getCourse } from '@/action/courseAction'
+import connectDB from '@/lib/bdconnect'
+import CourseModel from '@/models/courseModel';
 // import courses from '@/data/course.json'
 
 export async function GET(request) {
 
-    const fata = await getCourse();
-    // console.log(fata)
-    
-    const  arr = fata.data    
+    await connectDB();
+    const data = await CourseModel.find().lean();
 
-    return new Response(JSON.stringify(arr))
+
+
+    // const fata = await getCourse();
+    // // console.log(fata)
+    // const  arr = fata.data    
+
+    return new Response(JSON.stringify(data))
 }
-  
--- models/courseModel.js
+++ models/courseModel.js
@@ -1,13 +1,76 @@
-import { Schema, model, models } from 'mongoose'
 
 
-const courseSchema = new Schema ({
-    msg:{ 
-        type : String,
-        required : true
+import { Schema, model, models } from 'mongoose';
+
+const courseSchema = new Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: String,
+        required: true
+    },
+    difficulty: {
+        type: String,
+        required: true
+    },
+    instructor: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
     }
-}, {timestamps:true})
+}, { timestamps: true });
 
-const CourseModel = models.courses || model('courses',courseSchema)
+const CourseModel = models.courses || model('courses', courseSchema);
 
 export default CourseModel;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Schema, model, models } from 'mongoose'
+
+
+// const courseSchema = new Schema ({
+//     msg:{ 
+//         type : String,
+//         required : true
+//     }
+// }, {timestamps:true})
+
+// const CourseModel = models.courses || model('courses',courseSchema)
+
+// export default CourseModel;
+
+
+
