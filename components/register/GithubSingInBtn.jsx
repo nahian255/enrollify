232b6ffa76b58644b7ad4a76701bc6a4956fe5c8
Mutawchid Nahian <nahimn253@gmail.com>
2024-06-15 00:47:50 +0600

    make aroute

--- components/register/GithubSingInBtn.jsx
+++ components/register/GithubSingInBtn.jsx
@@ -4,11 +4,10 @@ import React from 'react';
 const GithubSingInBtn = () => {
     return (
         <div>
-            <button
-                onClick={() => signIn('github')}
-                className="w-full py-3 bg-gray-800 text-white font-bold rounded-md hover:bg-gray-900 focus:outline-none focus:ring focus:ring-gray-200"
-            >
-                Sign in with GitHub
+            <button  type='submit' name='action' value={'google'}
+            // onClick={() => signIn('github')}
+                className="w-full p-3 bg-gray-800 text-white font-bold rounded-md hover:bg-gray-900 focus:outline-none focus:ring focus:ring-gray-200"  >
+                 GitHub
             </button>
         </div>
     );
--- components/register/GoogleSingInBtn.jsx
+++ components/register/GoogleSingInBtn.jsx
@@ -4,12 +4,9 @@ import React from 'react';
 const GoogleSingInBtn = () => {
     return (
         <div>
-             <button
-                        onClick={() => signIn('google')}
-                        className="w-full py-3 bg-red-500 text-white font-bold rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-200"
-                    >
-                        Sign in with Google
-                    </button>
+            <button onClick={() => signIn('google')}
+                className="w-full p-3 bg-red-500 text-white font-bold rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-200"
+            > Google </button>
         </div>
     );
 };
