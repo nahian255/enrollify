4b5c2de17343601e7709af53c400b2fba837bde3
Mutawchid Nahian <nahimn253@gmail.com>
2024-07-13 17:50:43 +0600

    newly start, after recovery pc

--- app/blog/page.js
+++ app/blog/page.js
@@ -5,7 +5,7 @@ import { Button } from 'antd';
 const blogPage = () => {
     return (
         <div>
-            <h1>blog pages</h1>
+            <h1>blog pages .... content comming soon</h1>
             <Button type="primary">Button</Button>
         </div>
     );
