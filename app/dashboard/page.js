e1735964ded8cd3f36501415d6070e9e46f0d6ee
Mutawchid Nahian <nahimn253@gmail.com>
2024-07-13 18:26:27 +0600

    dashboard page, information added.

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,37 @@
-import React from 'react';
+'use client';
 
-const dashboard = () => {
+import React, { useEffect, useState } from 'react';
+
+const Dashboard = () => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    // get user from local store
+    useEffect(() => {
+        const user = localStorage.getItem('currentUser');
+        if (user) {
+            setCurrentUser(JSON.parse(user));
+        }
+    }, []);
+
     return (
-        <div>
-            <h1> dashboard </h1>
+        <div className='p-6 bg-red-300'>
+            <div>
+                <h1>Information</h1>
+                {currentUser ? (
+                    <div>
+                        <p><strong>Username:</strong> {currentUser.user.username}</p>
+                        <p><strong>Email:</strong> {currentUser.user.email}</p>
+                    </div>
+                ) : (
+                    <p>Loading user information...</p>
+                )}
+            </div>
+            <div>
+                <h1>Content</h1>
+                {/* Add your content here */}
+            </div>
         </div>
     );
 };
 
-export default dashboard;
+export default Dashboard;
