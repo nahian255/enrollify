7855a4af2908228162bf6b2ba8b75b7d14176284
Mutawchid Nahian <nahimn253@gmail.com>
2024-07-13 21:15:28 +0600

    add more link in dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -18,23 +18,33 @@ const Dashboard = () => {
         <div className='p-10 flex'>
 
             {/*  Information  */}
-            <div className=' w-1/3 bg-purple-100 rounded-2xl'>
+            <div className=' w-1/3 bg-purple-10 rounded-2xl'>
                 <h1 className="text-4xl font-serif font-semibold text-center"> Informa<span className="text-purple-500">tion</span> </h1>
                 {currentUser ? (
                     <div className='p-8'>
                         <p><strong>Username:</strong> {currentUser.user.username}</p>
                         <p><strong>Email:</strong> {currentUser.user.email}</p>
                         <hr className='my-4 border-2 border-purple-500 rounded' />
-                       
-                        <div className=' space-x-4 bg-slate-300'>
+
+                        <div className='flex flex-col gap-2'>
                             <Link href="/profile">
-                                <button className='bg-purple-500 text-white py-6 px-10 w-full rounded hover:bg-purple-600 transition-colors'>
+                                <button className='bg-purple-500 text-white py-4 w-full rounded hover:bg-purple-600 transition-colors'>
                                     My Profile
                                 </button>
                             </Link>
                             <Link href="/payment">
-                                <button className='bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition-colors'>
-                                    Payment
+                                <button className='bg-green-500 text-white py-4 w-full rounded hover:bg-green-600 transition-colors'>
+                                    Payment History
+                                </button>
+                            </Link>
+                            <Link href="/profile">
+                                <button className='bg-purple-500 text-white py-4 w-full rounded hover:bg-purple-600 transition-colors'>
+                                    All Classes
+                                </button>
+                            </Link>
+                            <Link href="/payment">
+                                <button className='bg-green-500 text-white py-4 w-full rounded hover:bg-green-600 transition-colors'>
+                                  My Wishlist
                                 </button>
                             </Link>
                         </div>
